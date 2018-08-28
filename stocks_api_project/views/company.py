8684900e78d0b92145318f03c2d8b8a40bcf38f9
MADELINE PETERS <madelinepet@hotmail.com>
2018-08-27 17:36:42 -0700

    Leaving for the day. Some things in company.py, readme.updated

--- stocks_api_project/views/company.py
+++ stocks_api_project/views/company.py
@@ -1,29 +1,29 @@
-# from pyramid_restful.viewsets import APIViewSet
-# from pyramid.response import Response
+from pyramid_restful.viewsets import APIViewSet
+from pyramid.response import Response
 
 
-#  @view_config(route_name='lookup', renderer='json', request_method='GET')
-#     def lookup(request):
-#     """ Makes the 3rd party api call
-#     """
-#     # insert base URL here
-#     url = 'postgres://localhost:5432'
-#     response = request.get(url)
-#     return Response(json=response.json(), status=200)
-
-# class CompanyAPIViewset(APIViewSet):
+ @view_config(route_name='lookup', renderer='json', request_method='GET')
+    def lookup(request):
+    """ Makes the 3rd party api call
+    """
+    # insert base URL here
+    url = f'https://api.iextrading.com/1.0//stock/{request.matchdict['symbol']}/book'
+    response = request.get(url)
+    return Response(json=response.json(), status=200)
 
 
-#     def retrieve(self, request, id=None):
-#         # http://localhost:6543/api/v1/company/{id}/
-#         # use the id to lookup that resource in the DB
-#         # formulate a response and send it back to the client
-#         return Response(
-#             json={'message': 'Provided a single resource'},
-#             status=200
-#             )
+class CompanyAPIViewset(APIViewSet):
 
-#     # An example
-#     # def list_all_companies(self, request):
-#     #     # http://localhost:6543/api/v1/company/
+    def retrieve(self, request, id=None):
+        # http://localhost:6543/api/v1/company/{id}/
+        # use the id to lookup that resource in the DB
+        # formulate a response and send it back to the client
+        return Response(
+            json={'message': 'Provided a single resource'},
+            status=200
+            )
+
+    # An example
+    # def list_all_companies(self, request):
+    #     # http://localhost:6543/api/v1/company/
 
--- stocks_api_project/views/stocks.py
+++ stocks_api_project/views/stocks.py
@@ -1,16 +1,29 @@
 from ..models.schemas import StocksSchema
+from ..models import Stock
+from sqlalchemy.exc import IntegrityError, DataError
 from pyramid_restful.viewsets import APIViewSet
 from pyramid.response import Response
 from pyramid.view import view_config
 import requests
-
+import json
 
 
 class StocksAPIViewset(APIViewSet):
     def create(self, request):
         """ creates a resource
         """
-        return Response(json={'message': 'Created a new record'}, status=201)
+        try:
+            kwargs = json.loads(request.body)
+        except json.JSONDecodeError as e:
+            return Response(json=e.message, status=400)
+        try:
+            stock = Stock.new(requests, **kwargs)
+        except IntegrityError:
+            return Response(json='Duplicate Key Error. Stock already exists', status=409)
+        schema = StocksSchema()
+        data = schema.dump(stock).data
+
+        return Response(json=data, status=201)
 
     def list(self, request):
         """ Gets all
@@ -28,12 +41,12 @@ class StocksAPIViewset(APIViewSet):
         if not id:
             return Response(status=204)
 
-        # try:
-        #  WeatherLocation.remove(request=request, pk=id)
+        try:
+            Stock.remove(request=request, pk=id)
 
-        # except(DataError, AttributeError):
-            # return Response(json'Not Found', status=404)
-        # return Response(status=204)
+        except(DataError, AttributeError):
+                return Response(json='Not Found', status=404)
+        return Response(status=204)
 
 
 
